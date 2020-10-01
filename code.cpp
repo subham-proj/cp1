723552681c984c5a64cc0b934baa2eac6badc986
Subham Singh <57002269+subham-proj@users.noreply.github.com>
2020-10-01 19:17:01 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,73 +1,46 @@
-#include <bits/stdc++.h>
-#define ll long long int
-#define mod 1000000007
-#define f(i,a,b) for(ll i=a;i<b;i++)
-#define rf(i,a,b) for(ll i=a;i>=b;i--)
-#define rep(i,n) f(i,0,n)
-#define rrep(i,n) rf(i,n-1,0)
-#define w(t) ll t; cin>>t; while(t--)
-#define vi vector<ll>
-#define mp make_pair
-#define pb push_back
-#define ff first
-#define ss second
-#define mii map<ll,ll>
-#define mci map<char,ll>
-using namespace std;
+#include<bits/stdc++.h> 
+using namespace std; 
 
-void fast_IO_file()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
-}
+int power(int x, unsigned int y) 
+{ 
+    if( y == 0) 
+        return 1; 
+    if (y%2 == 0) 
+        return power(x, y/2)*power(x, y/2); 
+    return x*power(x, y/2)*power(x, y/2); 
+} 
 
-int main()
-{
-	fast_IO_file();
+int order(int x) 
+{ 
+    int n = 0; 
+    while (x) 
+    { 
+        n++; 
+        x = x/10; 
+    } 
+    return n; 
+} 
 
-	int flag = 0;
-	string s, c = "";
-	cin >> s;
+bool isArmstrong(int x) 
+{ 
 
-	int n = s.length();
+    int n = order(x); 
+    int temp = x, sum = 0; 
+    while (temp) 
+    { 
+        int r = temp%10; 
+        sum += power(r, n); 
+        temp = temp/10; 
+    } 
+ 
+    return (sum == x); 
+} 
 
-	rep(i, n)
-	{
-
-
-		if (i < n - 2 && s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B')
-		{
-
-
-			if (flag != 1)
-			{
-				c += ' ';
-
-			}
-
-			i += 2;
-
-			flag = 1;
-		}
-		else
-		{
-			flag = 0;
-			c += s[i];
-		}
-	}
-	int d = 0;
-
-	if (c[0] == ' ')
-		d = 1;
-
-
-	f(i, d, c.length())
-	cout << c[i];
-
-
-	return 0;
-}
+int main() 
+{ 
+    int x = 153; 
+    cout << isArmstrong(x) << endl; 
+    x = 1253; 
+    cout << isArmstrong(x) << endl; 
+    return 0; 
+} 
